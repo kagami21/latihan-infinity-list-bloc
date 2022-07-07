31d9e11052d00664e88b2eeb0d094aeb5a987e44
Danu Nugroho <46646292+kagami21@users.noreply.github.com>
2022-07-07 16:46:49 +0700

    Add files via upload

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import './posts/posts.dart';
+
+class App extends MaterialApp{
+  App() : super(home: PostsPage());
+  }
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/widgets.dart';
+import './app.dart';
+import './simple_bloc_observer.dart';
+
+void main(){
+  BlocOverrides.runZoned(
+    () => runApp(App()),
+    blocObserver: SimpleBlocObserver());
+}
